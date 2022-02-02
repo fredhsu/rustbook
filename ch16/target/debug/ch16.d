8073c5d1f99c03f4b887c0b5698a767eab8fae3f
Fred Hsu <fredlhsu@gmail.com>
2022-02-02 07:48:22 -0800

    remove cache

--- ch16/target/debug/ch16.d
+++
@@ -1,1 +0,0 @@
-/home/fredlhsu/rust/rustbook/ch16/target/debug/ch16: /home/fredlhsu/rust/rustbook/ch16/src/main.rs
--- ch16/target/debug/deps/ch16-3630aef0c25b04dc.d
+++
@@ -1,5 +0,0 @@
-/home/fredlhsu/rust/rustbook/ch16/target/debug/deps/ch16-3630aef0c25b04dc: src/main.rs
-
-/home/fredlhsu/rust/rustbook/ch16/target/debug/deps/ch16-3630aef0c25b04dc.d: src/main.rs
-
-src/main.rs:
